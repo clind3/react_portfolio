4df5f648731b4a40e1eb85134d234b978ed4cd88
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-09-28 22:57:30 -0400

    Create array of project info to be included in portfolio

---
+++ src/constants.js
@@ -0,0 +1,40 @@
+
+const projects = [
+    {
+        id: 1,
+        name: "What's Going On?",
+        deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
+        gitHubRepo: 'https://github.com/clind3/whats_going_on',
+        src: "CSSCheatSheet.PNG"
+    },
+    {
+        id: 2,
+        name: "What's Going On?",
+        deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
+        gitHubRepo: 'https://github.com/clind3/whats_going_on',
+        src: 'GoodEatsSite.PNG'
+    },
+    {
+        id: 3,
+        name: "What's Going On?",
+        deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
+        gitHubRepo: 'https://github.com/clind3/whats_going_on',
+        src: 'WhatsGoingOnSite.PNG'
+    },
+    {
+        id: 4,
+        name: "What's Going On?",
+        deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
+        gitHubRepo: 'https://github.com/clind3/whats_going_on',
+        src: 'WhatsGoingOnSite.PNG'
+    },
+    {
+        id: 5,
+        name: "What's Going On?",
+        deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
+        gitHubRepo: 'https://github.com/clind3/whats_going_on',
+        src: 'WhatsGoingOnSite.PNG'
+    }
+];
+
+export default projects;
