12683e557421158048871cb5b538b7d4283abaa1
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-10-02 21:22:59 -0400

    fixed routing of application

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, { useState } from "react";
 import HomePage from "./components/Homepage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AboutMe from './components/AboutMe'
+import Contact from './components/Contact'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './styles/main.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Homepage');
-  const pageChange = (page) => setCurrentPage(page);
-  const renderPages = () => {
-    // if (currentPage === 'Contact') {
-    //   return <Contact />;
-    // }
-    // if (currentPage === 'About') {
-    //   return <About />;
-    // }
-    if (currentPage === 'Homepage') {
-      return <HomePage />;
-    }
-  }
 
   return (
     <body>
-      <Header currentPage={currentPage} changePage={pageChange}/>
-      {renderPages()}
-      <Footer currentPage={currentPage} changePage={pageChange}/>
+      <Header />
+      <Router>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route exact path='/about-me'>
+          <AboutMe />
+        </Route>
+        <Route exact path='/contact-me'>
+          <Contact />
+        </Route>
+      </Router>
+      <Footer />
     </body>
    
   );
