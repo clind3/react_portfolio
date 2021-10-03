36562e42919058e1c00d54e71168e76e643525a7
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-10-02 21:24:30 -0400

    fix background and dependencies

--- src/constants.js
+++ src/constants.js
@@ -1,39 +1,45 @@
 
+// add a description, container color, etc.
 const projects = [
     {
         id: 1,
-        name: "What's Going On?",
-        deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
-        gitHubRepo: 'https://github.com/clind3/whats_going_on',
-        src: "CSSCheatSheet.PNG"
+        name: "CSS Cheat Sheet",
+        deployedLink: "https://clind3.github.io/CSSCheatsheet/",
+        gitHubRepo: 'https://github.com/clind3/CSSCheatSheet',
+        src: "CSSCheatSheet.PNG",
+        description: 'Clips of CSS code that can be copied by viewer to put in their personal code.'
     },
     {
         id: 2,
-        name: "What's Going On?",
+        name: "Good Eat's",
         deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
         gitHubRepo: 'https://github.com/clind3/whats_going_on',
-        src: 'GoodEatsSite.PNG'
+        src: 'GoodEatsSite.PNG',
+        description: 'Recipe site that allows filtering and even a sidebar to find restaurants in their area.'
     },
     {
         id: 3,
         name: "What's Going On?",
         deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
         gitHubRepo: 'https://github.com/clind3/whats_going_on',
-        src: 'WhatsGoingOnSite.PNG'
+        src: 'WhatsGoingOnSite.PNG',
+        description: 'Restaurant Review Site'
     },
     {
         id: 4,
         name: "What's Going On?",
         deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
         gitHubRepo: 'https://github.com/clind3/whats_going_on',
-        src: 'WhatsGoingOnSite.PNG'
+        src: 'WhatsGoingOnSite.PNG',
+        description: 'Restaurant Review Site'
     },
     {
         id: 5,
         name: "What's Going On?",
         deployedLink: "https://whats-going-on-nightout.herokuapp.com/",
         gitHubRepo: 'https://github.com/clind3/whats_going_on',
-        src: 'WhatsGoingOnSite.PNG'
+        src: 'WhatsGoingOnSite.PNG',
+        description: 'Restaurant Review Site'
     }
 ];
 
